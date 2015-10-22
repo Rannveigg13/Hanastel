53f5b10d19ed2c5996a726a2ab15d9c7103c9892
Rannveigg13 <rannveigg13@ru.is>
2015-10-22 23:09:26 +0000

    Autocomplete searchbox semi working

--- app/src/main/java/com/example/notandi/hanastel/SearchActivity.java
+++ app/src/main/java/com/example/notandi/hanastel/SearchActivity.java
@@ -1,17 +1,43 @@
 package com.example.notandi.hanastel;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Notandi on 22-Oct-15.
  */
 public class SearchActivity extends MainActivity{
 
+    ArrayList<String> ingredients = new ArrayList<String>();
+    ArrayAdapter ingredientAdapter;
+    TextView searchBox;
+
+    String[] drinks = {"Sex on the beach", "Gin and Tonic", "White Russian",
+                        "Some fokking drink"};
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        // setup
+        searchBox = (TextView) findViewById(R.id.search_Box);
+
+        AutoCompleteTextView actv = (AutoCompleteTextView) findViewById(R.id.search_Box);
+        actv.setAdapter(new ArrayAdapter<String>(this, R.layout.autocomplete_list, drinks));
+
+        ingredientAdapter = new ArrayAdapter<String>(this, R.layout.ingredient_list, ingredients);
+        ListView ingr = (ListView) findViewById(R.id.ingredient_list);
+        ingr.setAdapter(ingredientAdapter);
     }
 
     @Override
@@ -22,5 +48,17 @@ public class SearchActivity extends MainActivity{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return false;
+    }
+
+    public void chooseIngredient(View view){
+        /*
+        TextView item = (TextView) findViewById(R.id.autocomplete_ingredient);
+        Log.d("clickInfo", item.getText().toString());
+        */
+        ingredients.add("Vodki");
+        ingredients.add("Romm");
+        Log.d("ingr", ingredients.toString());
+        ingredientAdapter.notifyDataSetChanged();
+        searchBox.setText("");
     }
 }
