d834aac230e989aca6bfc67f6d996032e2b4d18d
Rannveigg13 <rannveigg13@ru.is>
2015-10-30 06:25:11 +0000

    nothing

--- app/src/main/java/com/example/notandi/hanastel/MainActivity.java
+++ app/src/main/java/com/example/notandi/hanastel/MainActivity.java
@@ -81,7 +81,6 @@ public class MainActivity extends AppCompatActivity {
         {
             do{
                 Log.d("ITEMS/TABLES", c.getString(0));
-
             }while (c.moveToNext());
         }*/
     }
